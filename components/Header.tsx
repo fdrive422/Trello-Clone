5bc6a92c418a3389120c6903d7a0d46247ac9b64
fdrive422 <fafrago@gmail.com>
2023-09-08 20:54:09 -0700

    format header w/search bar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,39 @@
+"use client";
+
 import Image from "next/image";
+import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid";
+import Avatar from "react-avatar";
 
 function Header() {
   return (
     <header>
-      <Image
-        src="https://links.papareact.com/c2cdd5"
-        alt="Trello Logo"
-        width={300}
-        height={100}
-        className="w-44 md:w-56 pb-10 mb:pb-0 object-contain"
-      />
+      <div className="flex flex-col md:flex-row p-5 items-center bg-gray-500/10 rounded-b-2xl">
+        <Image
+          src="https://links.papareact.com/c2cdd5"
+          alt="Trello Logo"
+          width={300}
+          height={100}
+          className="w-44 md:w-56 pb-10 md:pb-0 object-contain"
+        />
+
+        <div className="flex items-center space-x-5 flex-1 justify-end w-full">
+          {/* Search Box */}
+          <form className="flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial">
+            <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
+            <input
+              type="text"
+              placeholder="Search"
+              className="flex-1 outline-none p-2"
+            />
+            <button type="submit" hidden>
+              Search
+            </button>
+          </form>
+
+          {/* Avatar */}
+          <Avatar name="Francis Frago" round size="50" color="#0055D1" />
+        </div>
+      </div>
     </header>
   );
 }
