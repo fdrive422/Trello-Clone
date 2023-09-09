570466393770d8d6fb91e568157d67bfca5269f8
fdrive422 <fafrago@gmail.com>
2023-09-08 20:05:44 -0700

    add header image:

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,17 @@
+import Image from "next/image";
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <Image
+        src="https://links.papareact.com/c2cdd5"
+        alt="Trello Logo"
+        width={300}
+        height={100}
+        className="w-44 md:w-56 pb-10 mb:pb-0 object-contain"
+      />
+    </header>
+  );
 }
 
 export default Header;
