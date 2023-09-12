9dac90464d3de2a4b14ae7f1617b94717a11f4e7
fdrive422 <fafrago@gmail.com>
2023-09-11 20:23:13 -0700

    setup appwrite db and app schema

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,5 +1,24 @@
+"use client";
+
+import { useBoardStore } from "@/store/BoardStore";
+import { useEffect } from "react";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
+
 function Board() {
-  return <div>Board</div>;
+  const getBoard = useBoardStore((state) => state.getBoard);
+
+  useEffect(() => {
+    getBoard();
+  }, [getBoard]);
+
+  return (
+    <h1>Hello</h1>
+    // <DragDropContext>
+    //   <Droppable droppableId="board" direction="horizontal">
+    //     {(provided) => <div>{/* rendering all the columns */}</div>}
+    //   </Droppable>
+    // </DragDropContext>
+  );
 }
 
 export default Board;
