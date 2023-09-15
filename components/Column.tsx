3b57abd3eb43488e4e2c9d30ce3321cca50b7633
fdrive422 <fafrago@gmail.com>
2023-09-14 21:42:45 -0700

    update board and setup column cards

---
+++ components/Column.tsx
@@ -0,0 +1,49 @@
+import { Draggable, Droppable } from "react-beautiful-dnd";
+
+type Props = {
+  id: TypedColumn;
+  todos: Todo[];
+  index: number;
+};
+
+const idToColumnText: {
+  [key in TypedColumn]: string;
+} = {
+  todo: "To Do",
+  inprogress: "In Progress",
+  done: "Done",
+};
+
+function Column({ id, todos, index }: Props) {
+  return (
+    <Draggable draggableId={id} index={index}>
+      {(provided) => (
+        <div
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+        >
+          {/* render droppable todos in the column */}
+          <Droppable droppableId={index.toString()} type="card">
+            {(provided, snapshot) => (
+              <div
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                className={`p-2 rounded-2xl shadow-sm ${
+                  snapshot.isDraggingOver ? "bg-green-200" : "bg-white/50"
+                }`}
+              >
+                <h2>{idToColumnText[id]}</h2>
+                <span className="text-gray-500 bg-gray-200 rounded-full px-2 py-2 text-sm">
+                  {todos.length}
+                </span>
+              </div>
+            )}
+          </Droppable>
+        </div>
+      )}
+    </Draggable>
+  );
+}
+
+export default Column;
--- lib/getTodosGroupedByColumn.ts
+++ lib/getTodosGroupedByColumn.ts
@@ -6,29 +6,53 @@ export const getTodosGroupedByColumn = async () => {
     process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!
   );
 
-  // const todos = data.documents;
+  const todos = data.documents;
 
-  console.log(data);
+  // console.log(todos);
 
-  // const columns = todos.reduce((acc, todo) => {
-  //   if (!acc.get(todo.status)) {
-  //     acc.set(todo.status, {
-  //       id: todo.status,
-  //       todos: [],
-  //     });
-  //   }
+  const columns = todos.reduce((acc, todo) => {
+    if (!acc.get(todo.status)) {
+      acc.set(todo.status, {
+        id: todo.status,
+        todos: [],
+      });
+    }
 
-  //   acc.get(todo.status)!.todos.push({
-  //     $id: todo.$id,
-  //     $createdAd: todo.$createdAt,
-  //     title: todo.title,
-  //     status: todo.status,
-  //     // get the image if it exists on the todo
-  //     ...(todo.image && { image: JSON.parse(todo.image) }),
-  //   });
+    acc.get(todo.status)!.todos.push({
+      $id: todo.$id,
+      $createdAd: todo.$createdAt,
+      title: todo.title,
+      status: todo.status,
+      // get the image if it exists on the todo
+      ...(todo.image && { image: JSON.parse(todo.image) }),
+    });
 
-  //   return acc;
-  // }, new Map<TypedColumn, Column>());
+    return acc;
+  }, new Map<TypedColumn, Column>());
 
   // console.log(columns);
+
+  // if columns does not have inprogress, todo, and done, add them with empty todos
+  const columnTypes: TypedColumn[] = ["todo", "inprogress", "done"];
+  for (const columnType of columnTypes) {
+    if (!columns.get(columnType)) {
+      columns.set(columnType, {
+        id: columnType,
+        todos: [],
+      });
+    }
+  }
+
+  // sort columns by column type
+  const sortedColumns = new Map(
+    Array.from(columns.entries()).sort(
+      (a, b) => columnTypes.indexOf(a[0]) - columnTypes.indexOf(b[0])
+    )
+  );
+
+  const board: Board = {
+    columns: sortedColumns,
+  };
+
+  return board;
 };
